d51a8bc6ad35b995117649d0a93fd1149135dffe
Vijendra Mishra <vmishra8@sapient.com>
2020-04-11 02:06:37 +0530

    added loader for content

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useEffect } from "react";
 import DataContext from "../src/context/DataContext";
 import reducer from "../src/reducers/reducer";
 import Container from "../src/components/organisms/Container";
+import Loader from "../src/components/atoms/Loader";
 import "./App.css";
 
 const initialState = {
-    isLoading: false,
+    isLoading: true,
     isError: false,
     data: null,
 };
@@ -14,6 +15,9 @@ function App() {
     const [state, dispatch] = React.useReducer(reducer, initialState);
 
     useEffect(() => {
+        dispatch({
+            type: "FETCH_DATA_PENDING",
+        });
         fetch("http://hn.algolia.com/api/v1/search?query=foo&tags=story")
             .then((res) => res.json())
             .then(
@@ -21,6 +25,9 @@ function App() {
                     dispatch({
                         type: "SET_DATA",
                         payload: results,
+                    });
+                    dispatch({
+                        type: "FETCH_DATA_COMPLETE",
                     });
                 },
                 (error) => {
@@ -31,7 +38,9 @@ function App() {
                 }
             );
     }, []);
-    return (
+    return state.isLoading ? (
+        <Loader />
+    ) : (
         <DataContext.Provider
             value={{
                 state,
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,16 @@
 const reducer = (state, action) => {
     const { type, payload } = action;
     switch (type) {
+        case "FETCH_DATA_PENDING":
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case "FETCH_DATA_COMPLETE":
+            return {
+                ...state,
+                isLoading: false,
+            };
         case "SET_DATA":
             return {
                 ...state,
